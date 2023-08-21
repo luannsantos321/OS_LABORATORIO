238eb23f34f05684f5dc2d3962f2dfa604367172
Luann Rodrigo de Souza Santos <94115637+luannsantos321@users.noreply.github.com>
2023-08-21 11:59:48 -0300

    Update main.py

--- main.py
+++ main.py
@@ -25,8 +25,13 @@ opcoes = st.multiselect(
 st.write('Itens:', opcoes)
 
 
-n_serie = st.text_input('Numero de série do item:')
-st.write('Numero de série do item:', n_serie)
+n_maquina = st.text_input('Numero de série da Máquina:')
+st.write('Numero de série do item:', n_maquina)
+n_clivador = st.text_input('Numero de série do Clivador:')
+st.write('Numero de série do item:', n_maquina)
+n_power = st.text_input('Numero de série do Power Meter:')
+st.write('Numero de série do item:', n_maquina)
+
 
 outros_itens = st.text_input('Outros itens:')
 st.write('Outros itens:', outros_itens)
@@ -40,7 +45,7 @@ st.write('Ramais:', ramais)
 
 traco = '-'* 45
 
-os = f'Quem entregou: {entregando}\n\nAss: {traco}\n\nAutorizado a pegar: {autorizado}\n\nAss: {traco}\n\nPosto de teste de atendimento: {posto_atendimento}\nPosto de teste da manutenção: {posto_manutencao}\nEventuais problemas: {txt}\nItens da maleta: {opcoes}\nOutros itens: {outros_itens}\nNumero de série do item: {n_serie}\nRamal(s): {ramais} '
+os = f'Quem entregou: {entregando}\n\nAss: {traco}\n\nAutorizado a pegar: {autorizado}\n\nAss: {traco}\n\nPosto de teste de atendimento: {posto_atendimento}\nPosto de teste da manutenção: {posto_manutencao}\nEventuais problemas: {txt}\nItens da maleta: {opcoes}\nOutros itens: {outros_itens}\nNumero de série da Máquina: {n_maquina}\nNumero de série do Clivador: {n_clivador}\nNumero de série do Power Meter: {n_power}\nRamal(s): {ramais} '
 
 st.download_button('Download',os, file_name = f'{entregando} {formatado}')
 
